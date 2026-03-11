64a892ff502896202c9fe5cde4073eff530be50a
shveni21 <shvenireddy@gmail.com>
2026-03-11 23:45:20 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>UI/UX Mockup Generator</h2>
+      <Button>Subscribe</Button>
     </div>
   );
 }
